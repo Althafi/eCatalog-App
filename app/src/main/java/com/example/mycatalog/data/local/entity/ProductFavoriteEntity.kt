88f99a4ceed671102115c5ab3efe0cca099befaf
Althafi Hilal Anshar <altp.hlnshr14@gmail.com>
2024-02-03 16:05:00 +0700

    commit

--- app/src/main/java/com/example/mycatalog/data/local/entity/ProductFavoriteEntity.kt
+++ app/src/main/java/com/example/mycatalog/data/local/entity/ProductFavoriteEntity.kt
@@ -1,10 +1,8 @@
 package com.example.mycatalog.data.local.entity
 
-import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 
 
 const val TABLE_PRODUCTS = "product_favorite"
